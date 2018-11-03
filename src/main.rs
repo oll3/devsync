1627a2a1a76100f9e399ea7b72e69b7bc649dbce
Olle Sandberg <olle@b3rg.se>
2018-11-03 16:01:58 +0100

    Implement the synchronize part.
    Also allow to read source data from stdin if no source file is given.

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,27 @@ use getopts::Options;
 use std::cmp;
 use std::env;
 use std::fs::File;
+use std::fs::OpenOptions;
+use std::io;
 use std::io::prelude::*;
 use std::io::{Seek, SeekFrom};
 use std::process;
 
 #[derive(Debug)]
 struct Config {
-    input: String,
-    output: String,
+    src: Option<String>,
+    dest: String,
     block_size: usize,
     buf_size: usize,
     dry_run: bool,
 }
 
 fn print_usage(program: &str, opts: Options) {
-    let brief = format!("Usage: {} <input> <output> [options]", program);
+    let brief = format!("Usage: {} <destination file> [options]", program);
     print!("{}", opts.usage(&brief));
     println!();
     println!("    SIZE values can be given in units 'TiB', 'GiB', 'MiB', 'KiB', or 'B' (default).");
+    println!("    Will run until reaches end of file of either source or destionation.");
     println!();
 }
 
@@ -49,7 +52,18 @@ fn parse_opts() -> Config {
     let mut opts = Options::new();
 
     opts.optopt("b", "block-size", "set block-size (default 1KiB)", "SIZE");
-    opts.optopt("s", "buffer-size", "set buffer-size (default 1KiB)", "SIZE");
+    opts.optopt(
+        "u",
+        "buffer-blocks",
+        "number of blocks to buffer (default 1)",
+        "BLOCKS",
+    );
+    opts.optopt(
+        "s",
+        "source",
+        "source of synchronization (default is stdin)",
+        "FILE",
+    );
     opts.optflag("d", "dry-run", "compare but do not write");
     opts.optflag("h", "help", "print this help menu");
     let matches = match opts.parse(&args[1..]) {
@@ -60,101 +74,123 @@ fn parse_opts() -> Config {
         print_usage(&program, opts);
         process::exit(1);
     }
+    let src = matches.opt_str("s");
     let block_size = if let Some(ref val) = matches.opt_str("b") {
         parse_size(val)
     } else {
         1024
     };
-    let buf_size = if let Some(ref val) = matches.opt_str("s") {
-        parse_size(val)
+    let buf_blocks: usize = if let Some(ref val) = matches.opt_str("u") {
+        val.parse().expect("blocks")
     } else {
-        1024
+        1
     };
-    if buf_size < block_size {
-        panic!("buffer size < block size");
-    }
     let dry_run = matches.opt_present("d");
-    let (input, output) = if matches.free.len() == 2 {
-        (matches.free[0].clone(), matches.free[1].clone())
+    let dest = if matches.free.len() == 1 {
+        matches.free[0].clone()
     } else {
         print_usage(&program, opts);
         process::exit(1);
     };
 
     Config {
-        input: input,
-        output: output,
+        src: src,
+        dest: dest,
         dry_run: dry_run,
         block_size: block_size,
-        buf_size: buf_size,
+        buf_size: buf_blocks * block_size,
     }
-}
-
-fn min_file_size(in_file: &mut File, out_file: &mut File) -> u64 {
-    let ir = in_file.seek(SeekFrom::End(0)).expect("seek in file");
-    let or = out_file.seek(SeekFrom::End(0)).expect("seek out file");
-    println!("ir={:?}, or={:?}", ir, or);
-    in_file.seek(SeekFrom::Start(0)).expect("seek in file");
-    out_file.seek(SeekFrom::Start(0)).expect("seek out file");
-
-    return cmp::min(ir, or);
-}
-
-struct BlockDesc {
-    block: usize,
-    offset: std::io::SeekFrom,
-    size: usize,
 }
 
 fn size_to_str(size: &usize) -> String {
     if *size > 1024 * 1024 {
-        format!("{} MiB ({} B)", size / (1024 * 1024), size)
+        format!("{} MiB ({} bytes)", size / (1024 * 1024), size)
     } else if *size > 1024 {
-        format!("{} KiB ({} B)", size / (1024), size)
+        format!("{} KiB ({} bytes)", size / (1024), size)
     } else {
-        format!("{} B", size)
+        format!("{} bytes", size)
     }
 }
 
-// Compare two files and return list of blocks that differ between them
-fn diff_file(config: &Config, in_file: &mut File, out_file: &mut File) -> Vec<BlockDesc> {
+// Fill buffer with data from file, or until eof.
+fn fill_buf<T>(file: &mut T, buf: &mut Vec<u8>) -> usize
+where
+    T: Read,
+{
+    let mut read_size = 0;
+    let buf_size = buf.len();
+    while read_size < buf_size {
+        let rc = file.read(&mut buf[read_size..buf_size]).expect("read");
+        if rc == 0 {
+            break;
+        } else {
+            read_size += rc;
+        }
+    }
+    return read_size;
+}
+
+// Compare and sync two files
+fn sync_files<T>(config: &Config, src_file: &mut T, dest_file: &mut File)
+where
+    T: Read,
+{
+    let mut diff_blocks = 0;
     let mut diff_bytes = 0;
     let mut total_bytes = 0;
-    let mut differ = vec![];
     let mut block_cnt = 0;
-    let mut in_buf: Vec<u8> = vec![0; config.buf_size as usize];
-    let mut out_buf: Vec<u8> = vec![0; config.buf_size as usize];
+
+    // Pre-allocate the buffers to use
+    let mut src_buf: Vec<u8> = vec![0; config.buf_size as usize];
+    let mut dest_buf: Vec<u8> = vec![0; config.buf_size as usize];
 
     loop {
-        let mut buf_offs = 0;
-
-        // Read at max 'buf_size' bytes from file
-        let in_rc = in_file.read(&mut in_buf).expect("read input file");
-        let out_rc = out_file.read(&mut out_buf).expect("read input file");
-        if in_rc == 0 || out_rc == 0 {
+        // Fill both buffers to be used for comparsion.
+        let src_buf_size = fill_buf(src_file, &mut src_buf);
+        let dest_buf_size = fill_buf(dest_file, &mut dest_buf);
+        if src_buf_size == 0 || dest_buf_size == 0 {
             // Reached eof for one of the files
             break;
         }
 
-        while buf_offs < in_rc && buf_offs < out_rc {
-            // Iterate and compare 'block_size' bytes (at max) each time
-            let in_cmp_size = cmp::min(config.block_size, in_rc - buf_offs);
-            let out_cmp_size = cmp::min(config.block_size, out_rc - buf_offs);
-            let cmp_size = cmp::min(in_cmp_size, out_cmp_size);
+        let mut buf_offs = 0;
+        while buf_offs < src_buf_size && buf_offs < dest_buf_size {
+            // Iterate and compare 'block_size' bytes (at max) each time.
+            // Only time the size of the source and the destination may differ
+            // is when reading the last block of one of them.
+            let src_cmp_size = cmp::min(config.block_size, src_buf_size - buf_offs);
+            let dest_cmp_size = cmp::min(config.block_size, dest_buf_size - buf_offs);
+            let cmp_size = cmp::min(src_cmp_size, dest_cmp_size);
 
-            let in_slice = &in_buf[buf_offs..buf_offs + cmp_size];
-            let out_slice = &out_buf[buf_offs..buf_offs + cmp_size];
+            let src_slice = &src_buf[buf_offs..(buf_offs + cmp_size)];
+            let dest_slice = &dest_buf[buf_offs..(buf_offs + cmp_size)];
 
-            let block_desc = BlockDesc {
-                block: block_cnt,
-                offset: SeekFrom::Start(block_cnt as u64 * config.block_size as u64),
-                size: cmp_size,
-            };
-
-            // Compare block
-            if in_slice != out_slice {
-                differ.push(block_desc);
+            // Compare the two block buffers and write the soruce to the
+            // destination file if they differ (and this is not a dry run).
+            if src_slice != dest_slice {
+                diff_blocks += 1;
                 diff_bytes += cmp_size;
+
+                if !config.dry_run {
+                    // Store current position of dest file
+                    let old_pos = dest_file.seek(SeekFrom::Current(0)).expect("seek current");
+
+                    // jump back to block start
+                    dest_file
+                        .seek(SeekFrom::Start((block_cnt * config.block_size) as u64))
+                        .expect("seek");
+
+                    // write in data to dest file
+                    let wc = dest_file.write(src_slice).expect("write");
+                    if wc != cmp_size {
+                        panic!("wc != cmp_size");
+                    }
+
+                    // restore position in dest file
+                    dest_file
+                        .seek(SeekFrom::Start(old_pos))
+                        .expect("seek restore");
+                }
             }
 
             block_cnt += 1;
@@ -164,32 +200,40 @@ fn diff_file(config: &Config, in_file: &mut File, out_file: &mut File) -> Vec<Bl
     }
 
     println!(
-        "Compared {} blocks, {} in total.",
+        "Compared {} blocks ({} in total).",
         block_cnt,
         size_to_str(&total_bytes)
     );
     println!(
-        "{} blocks differs, {} in total.",
-        differ.len(),
-        size_to_str(&diff_bytes)
+        "{} blocks differed ({} in total) {} written to destination.",
+        diff_blocks,
+        size_to_str(&diff_bytes),
+        if config.dry_run {
+            "but was NOT"
+        } else {
+            "and was"
+        }
     );
-    return differ;
 }
 
 fn main() {
     let config = parse_opts();
-    println!("config={:?}", config);
 
-    let mut in_file =
-        File::open(&config.input).expect(&format!("failed to open file ({})", config.input));
-    let mut out_file =
-        File::open(&config.output).expect(&format!("failed to open file ({})", config.input));
+    let mut dest_file = OpenOptions::new()
+        .read(true)
+        // Open with write access if not a 'dry run'
+        .write(config.dry_run == false)
+        .open(&config.dest)
+        .expect(&format!("failed to open file ({})", config.dest));
 
-    println!(
-        "Comparing blocks (size: {}) between '{}' and '{}'",
-        size_to_str(&config.block_size),
-        config.input,
-        config.output
-    );
-    let diff = diff_file(&config, &mut in_file, &mut out_file);
+    if let Some(val) = &config.src {
+        // Read from input file
+        let mut src_file = File::open(&val).expect(&format!("failed to open file ({})", val));
+        sync_files(&config, &mut src_file, &mut dest_file);
+    } else {
+        // Read from stdin
+        let stdin = io::stdin();
+        let mut src_file = stdin.lock();
+        sync_files(&config, &mut src_file, &mut dest_file);
+    }
 }
